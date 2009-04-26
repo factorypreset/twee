6d196c9c36bc43972251d629fbfa70595f381509
klimas@gmail.com <klimas@gmail.com@e208ffdb-013f-0410-a97a-fdaa23ae8a23>
2009-04-26 00:01:39 +0000

    adding manifest to get Vista themed controls; doesn't seem to work in Python 2.6, though.
    git-svn-id: http://twee.googlecode.com/svn@101 e208ffdb-013f-0410-a97a-fdaa23ae8a23

--- trunk/twine/buildexe.py
+++ trunk/twine/buildexe.py
@@ -2,8 +2,35 @@
 # This builds an .exe out of Tweebox for use with Windows.
 # Call this with this command line: buildexe.py py2exe
 
-import sys, os
+import sys, os, py2exe
 from distutils.core import setup
-import py2exe
 
-setup(windows=['app.py'])
+manifest = """
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1"
+manifestVersion="1.0">
+<assemblyIdentity
+    version="0.64.1.0"
+    processorArchitecture="x86"
+    name="Controls"
+    type="win32"
+/>
+<description>Twine</description>
+<dependency>
+    <dependentAssembly>
+        <assemblyIdentity
+            type="win32"
+            name="Microsoft.Windows.Common-Controls"
+            version="6.0.0.0"
+            processorArchitecture="X86"
+            publicKeyToken="6595b64144ccf1df"
+            language="*"
+        />
+    </dependentAssembly>
+</dependency>
+</assembly>
+"""
+
+# build the exe
+
+setup( windows = [{ 'script':'app.py', 'other_resources': [(24, 1, manifest)] }])
